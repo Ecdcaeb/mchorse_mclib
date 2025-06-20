09bdd9f419788bc39df850a3fc79ed06743f4be5
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-06-20 09:17:05 +0800

    Update McLibCMInfo.java
    fix:
    
    `java.lang.NullPointerException: Cannot read field "parentMod" because the return value of "net.minecraftforge.fml.common.ModContainer.getMetadata()" is null`
    
    ModMetadata should not be null

--- src/main/java/mchorse/mclib/core/McLibCMInfo.java
+++ src/main/java/mchorse/mclib/core/McLibCMInfo.java
@@ -1,30 +1,23 @@
 package mchorse.mclib.core;
 
+import com.google.common.eventbus.EventBus;
+import net.minecraftforge.fml.common.DummyModContainer;
+import net.minecraftforge.fml.common.LoadController;
+import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.DummyModContainer;
 
 public class McLibCMInfo extends DummyModContainer
 {
-    @Override
-    public String getName()
-    {
-        return "McLib core mod";
+    public McLibCMInfo() {
+        super(new ModMetadata());
+        ModMetadata meta = this.getMetadata();
+        meta.modId = "mclib_core";
+        meta.name = "McLib core mod";
+        meta.version = "%VERSION%";
     }
 
     @Override
-    public String getModId()
-    {
-        return "mclib_core";
-    }
-
-    @Override
-    public Object getMod()
-    {
-        return null;
-    }
-
-    @Override
-    public String getVersion()
-    {
-        return "%VERSION%";
+    public boolean registerBus(EventBus bus, LoadController controller) {
+        return true;
     }
 }
